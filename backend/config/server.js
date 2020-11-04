b08a0145a7ef018cd39a29b5b8386345900ce5fa
Elmar Wouters <elmar@elmarwouters.nl>
2020-11-04 01:08:25 +0100

    Added strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4ef41177ff7c30ab06b465665147e080'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3314a36-aee3-41e0-ac56-6b7f3df62e63'
+};
